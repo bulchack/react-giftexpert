91a8545ad434c4bf3bb0cd2a53af539199215af8
Jesus Ovalles <jesuseovalles@gmail.com>
2020-10-31 10:45:25 -0300

    docs agregado

---
+++ docs/precache-manifest.288eda0248e09f218afb24ae2041f3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e728a6113f4e48d69ac9590dfb66d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fb88e221c0e71a987bd",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "a45c0ff7c9e28121c691",
+    "url": "/static/js/2.3e75c129.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.3e75c129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb88e221c0e71a987bd",
+    "url": "/static/js/main.f497ddb8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
