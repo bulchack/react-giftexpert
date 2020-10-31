ca265510d944ca4a1d9721671d57306a043c8a40
Jesus Ovalles <jesuseovalles@gmail.com>
2020-10-31 10:44:02 -0300

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    const [categories, setcategories] = useState(['Futbol'])
+
+    // const handleApp = () => {
+    //     setcategories(
+    //         [...categories, 'Pruebita']
+    //     )
+    // }
+    return ( 
+        <>
+            <h2>
+                GifExpertApp
+            </h2>
+            <AddCategory setCategories={ setcategories }/>
+            <hr/>
+            <ol>
+                {
+                    categories.map( category => {
+                    return <GifGrid 
+                            key={ category }
+                            category={ category }/>
+                    })
+                }
+            </ol>
+        </>
+     );
+}
+ 
